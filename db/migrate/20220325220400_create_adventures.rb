7a7ec0382782df292f6ea537921a17228a53a46e
“Meg <meg5ed@virginia.edu>
2022-03-25 16:04:24 -0600

    added models

---
+++ db/migrate/20220325220400_create_adventures.rb
@@ -0,0 +1,10 @@
+class CreateAdventures < ActiveRecord::Migration[7.0]
+  def change
+    create_table :adventures do |t|
+      t.string :title
+      t.text :desc
+
+      t.timestamps
+    end
+  end
+end
